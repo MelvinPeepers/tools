d9671b328e4ce1a909ffdfb41578213a1aea8212
Melvin <peepersm@gmail.com>
2021-01-13 10:18:16 -0500

    Setup css for login page

--- my-app/src/components/pages/login/Login.js
+++ my-app/src/components/pages/login/Login.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { Fragment } from "react";
+import './Login.css';
 
 const Login = () => {
   return (
-    <form>
-      <input type="text" placeholder="Email" name="email"></input>
-      <input type="password" placeholder="Password" name="password"></input>
-      <input type="submit"></input>
-    </form>
+    <Fragment>
+      <div className="header-secondary">
+        <h2>Login to CC Tools</h2>
+      </div>
+      <div className="container-form">
+        <form>
+        <ul className="form-list">
+          <li className="list-input">
+          <label className='label' for="text-input">Username: </label>
+            <input className="input" type="text" placeholder="Email" name="email"></input>
+          </li>
+          <li className="list-input">
+          <label className='label' for="text-input">Password: </label>
+            <input className="input" type="password" placeholder="Password" name="password"></input>
+          </li>
+          <input className="form-input form-btn" type="submit"></input>
+          </ul>
+        </form>
+      </div>
+    </Fragment>
   )
 }
 
