0dcafd1b0e9a9e00104701b1d12b4156f94ff9d0
Melvin <peepersm@gmail.com>
2021-01-15 11:38:37 -0500

    added a method for the form

--- my-app/src/components/pages/numbers/Numbers.js
+++ my-app/src/components/pages/numbers/Numbers.js
@@ -29,7 +29,7 @@ const Numbers = () => {
       <div className="header-secondary">
         <h2>Strip and format numbers to 11 digits</h2>
         <div className="container-form">
-          <form name="number">
+          <form name="number" method="post">
             <ul className="form-list">
               <li className="list-input">
                 <label className='label' for="text-input">Input Number: </label>
