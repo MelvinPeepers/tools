0b023c5e54e484136890d892aaa6158a125d45e5
Melvin <peepersm@gmail.com>
2021-02-03 22:44:29 -0500

    login page"

--- my-app/src/components/pages/login/Login.js
+++ my-app/src/components/pages/login/Login.js
@@ -1,24 +1,26 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import './Login.css';
 
 const Login = () => {
+  const [name, setName] = useState('');
+
   return (
     <Fragment>
       <div className="header-secondary">
-        <h2>Login to CC Tools</h2>
+        <h2>Login to My Tools</h2>
       </div>
       <div className="container-form">
         <form>
-        <ul className="form-list">
-          <li className="form-input">
-          <label className="login-label">Username: </label>
-            <input className="login-input" type="text" name="email"></input>
-          </li>
-          <li className="form-input">
-          <label className="login-label">Password: </label>
-            <input className="login-input" type="text"></input>
-          </li>
-          <button className="login-btn">Login</button>
+          <ul className="form-list">
+            <li className="form-input">
+              <label className="login-label">Username: </label>
+              <input className="login-input" type="text" name="email"></input>
+            </li>
+            <li className="form-input">
+              <label className="login-label">Password: </label>
+              <input className="login-input" type="text"></input>
+            </li>
+            <button type="submit" className="login-btn">Login</button>
           </ul>
         </form>
       </div>
