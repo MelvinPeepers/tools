bead6a188d3e32006fd9b7f57ca9ba0aaa49daa3
Melvin <peepersm@gmail.com>
2021-02-07 13:07:11 -0500

    commited out unused code in login.js

--- my-app/src/components/pages/login/Login.js
+++ my-app/src/components/pages/login/Login.js
@@ -1,8 +1,8 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import './Login.css';
 
 const Login = () => {
-  const [name, setName] = useState('');
+  //const [name, setName] = useState('');
 
   return (
     <Fragment>
