b5f80c94c2ea4aecef590e42b3dcd8555b5874bb
Melvin <peepersm@gmail.com>
2021-02-11 12:09:19 -0500

    styling form and inputs on the password page

--- my-app/src/components/pages/passwordGen/PasswordGen.js
+++ my-app/src/components/pages/passwordGen/PasswordGen.js
@@ -8,7 +8,7 @@ const PasswordGen = () => {
         <h2>Password Generator</h2>
         <div className='container-form'>
           <div className='password-form'>
-            <label htmlFor='password-length'>Password Length</label>
+            <label className='password-label' htmlFor='password-length'>Password Length</label>
             <input type='number' id='password-length' name='password-length' min='6' max='15'  />
           </div>
           <div className='password-txt'>
@@ -16,8 +16,10 @@ const PasswordGen = () => {
             <p>Must contain at least two groups of characters. Either letters and numbers or capitalized letters and lowercase letters</p>
             <p>No sequences, and no special characters/symbols.</p>
           </div>
-          <button className='generator-btn'>Generate</button>
-          <button className='copy-btn'>Copy</button>
+          <div className='btn-group'>
+          <button className='generator-btn password-btn'>Generate</button>
+          <button className='copy-btn password-btn'>Copy</button>
+          </div>
         </div>
       </div>
     </Fragment>
