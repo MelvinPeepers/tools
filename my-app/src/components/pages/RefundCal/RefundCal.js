d57443e00346b8cbef9ebb7ea56b65e542142780
Melvin <peepersm@gmail.com>
2021-02-23 21:49:12 -0500

    reorganized

---
+++ my-app/src/components/pages/RefundCal/RefundCal.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import './RefundCal.css';
+
+const RefundCal = () => {
+
+  return (
+    <Fragment>
+      <div className="header-secondary">
+        <h2>Refund Calculator</h2>
+        <p>App to Refund Calculator</p>
+        <p>Version: 1.0.0 alpha</p>
+      </div>
+
+    </Fragment>
+  )
+}
+
+export default RefundCal
